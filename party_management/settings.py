cb25832931c1bd3155bc0580522ecb00b51ba76d
eudp-admin <infoeudp2017@gmail.com>
2025-10-07 18:30:53 +0300

    Final Cloudinary settings adjustments and migrations.

--- party_management/settings.py
+++ party_management/settings.py
@@ -104,7 +104,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # --- Media Files (Cloudinary) ---
 MEDIA_URL = '/media/'
